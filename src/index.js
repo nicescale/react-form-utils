a3c9ae6fbb89c158e1050d9a953f3f8c9aef6f03
tsing <tsing@jianqing.org>
2015-09-18 10:39:21 +0800

    Use bable to transpile files

---
+++ src/index.js
@@ -0,0 +1,11 @@
+var {Form, Field} = require('./Form');
+var {makeState, State} = require('./State');
+var FormMixin = require('./FormMixin');
+
+module.exports = {
+  Form,
+  Field,
+  makeState,
+  State,
+  FormMixin
+};
